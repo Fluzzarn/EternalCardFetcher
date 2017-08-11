f4fada323f099451715cf5267deac3642fee1f0b
fluzzarn <fluzzarn@gmail.com>
2017-08-11 17:31:14 -0400

    Initial Commit

---
+++ EternalCardFetcher/EternalBot.cs
@@ -0,0 +1,30 @@
+﻿using RedditSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EternalCardFetcher
+{
+    class EternalBot
+    {
+
+        string _subreddit = "";
+
+        public EternalBot(string subreddit)
+        {
+            _subreddit = subreddit;
+        }
+
+        public void StartBot()
+        {
+            var webAgent = new BotWebAgent("EternalCardBot")
+        }
+
+        public void ConsumeComments()
+        {
+            
+        }
+    }
+}
